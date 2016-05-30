1f58c6cbe7c8ad69d3388fcbfbdc093c42bf7ced
hmiranda3 <habibmiranda@gmail.com>
2016-05-30 09:31:52 -0600

    Stretch Commit

---
+++ SpareChangeStretch.playground/Contents.swift
@@ -0,0 +1,18 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var str = "Hello, playground"
+
+func change(amount: Double) {
+    let dollar = 1.0
+    let quarter = 0.25
+    let dime = 0.10
+    let nickel = 0.05
+    let penny = 0.01
+    
+    if
+    print ("\(floor(amount)/dollar), ")
+}
+
+change(2.15)
